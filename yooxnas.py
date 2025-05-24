f320955d16c1011e067a0f522ceac3c18f441f77
Jesse Wattenbarger <jwattenbarger@spencerhealthsolutions.com>
2025-05-24 10:40:29 -0400

    Add more tokens and start writing lexer functions

--- yooxnas.py
+++ yooxnas.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
 import argparse
 
+from enum import Enum, auto
 
 class TOKENTYPE(Enum):
-    PIPE = auto() # |
-    INT = auto()
-    AT = auto() # @
-    CHAR = auto()
-    AMP = auto()
-    DOLLAR = auto()
+    RUNE_PIPE = auto() # |
+    RUNE_AT = auto() # @
+    RUNE_PERIOD = auto()
+    RUNE_AMPERSAND = auto()
+    RUNE_DOLLAR = auto()
+    RUNE_SEMICOLON = auto()
+    RUNE_MINUS = auto()
+    RUNE_HASH = auto()
+    RUNE_QUESTION = auto()
+    RUNE_EXCLAIM = auto()
+    RUNE_BACKSLASH = auto()
+    RUNE_FORWARDSLASH = auto()
+    RUNE_UNDERSCORE = auto()
+    RUNE_COMMA = auto()
+    RUNE_EQUAL = auto()
+    RUNE_DOUBLE_QUOTE = auto()
+    RUNE_PERCENT = auto()
+    RUNE_TILDE = auto()
     LPAREN = auto ()
     RPAREN = auto()
-    SEMICOLON = auto()
-    MINUS = auto()
-    HASH = auto()
-    QUESTION = auto()
-    EXCLAIM = auto()
-    SLASH = auto()
+    HEX_LITERAL = auto()
+    IDENTIFIER = auto()
+    WHITESPACE = auto()
+    NEWLINE = auto()
+    COMMENT = auto()
+    EOF = auto()
+    ILLEGAL = auto()
+
 
 
 class Token:
-    def __init__(self, type: TOKENTYPE, word: str, line: int):
-        self.type = type
+    def __init__(self, typ: TOKENTYPE, word: str, line: int):
+        self.type = typ
         self.word = word
         self.line = line
 
@@ -33,9 +48,29 @@ class Lexer:
     def __init__(self, source: str):
         self.src = source
         self.size = len(source)
+        # Current position in the input
         self.cursor = 0
+        # Start position of the current token being scanned
         self.start = 0
         self.line = 1
+
+    def _is_at_end(self):
+        return self.cursor >= self.size
+
+    def _advance(self):
+        char = self.src[self.cursor]
+        self.cursor += 1
+        return char
+
+    def _peek(self):
+        if self._is_at_end:
+            return '\0'
+        return self.src[self.cursor]
+
+    def _peek_next(self):
+        if self.cursor + 1 >= self.size:
+            return '\0'
+        return self.src[self.cursor + 1]
 
 
 def parse_args() -> argparse.Namespace:
