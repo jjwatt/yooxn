ae8ed3ced92dee16252ead292fab2e2793b6d059
Jesse Wattenbarger <jwatt@broken.watch>
2025-06-14 13:48:18 -0400

    Updating test_yooxnas. Currently broken.

--- test_yooxnas.py
+++ test_yooxnas.py
@@ -1,12 +1,30 @@
+import unittest
+
 import yooxnas
 
-source = "|10 @Console &vector $2 &read $1 &pad $5 &write $1 &error $1\n|0100 ( -> )"
+def load_and_parse_file(input_filename, output_filename):
+    file_path = Path(filename)
+    with open(args.file, 'r') as asmfile:
+        source_code = asmfile.read()
+        lexer = Lexer(source_code, filename=file_path)
+        tokens = lexer.scan_all_tokens()
+        logger.debug("Finished tokenizing.")
 
-l = yooxnas.Lexer(source)
-tokens = l.scan_all_tokens()
+        if tokens and tokens[-1].type != TOKENTYPE.ILLEGAL:
+            parser = Parser(tokens, cur_filepath=file_path)
+            ir_stream, symbol_table = parser.parse_pass1()
+            try:
+                parser.parse_pass2(ir_stream, symbol_table)
+            except ParsingError:
+                logger.debug("Parsing error in Pass 2")
+                raise
+            parser.write_rom(args.output)
+        else:
+            print("Lexer failed. Parsing skipped.")
 
-for t in tokens:
-    t.print()
+class TestSimple(unittest.TestCase):
+    pass
 
-parser = yooxnas.Parser(tokens)
-parser.parse_pass1()
+
+if __name__ == "__main__":
+    unittest.main()
