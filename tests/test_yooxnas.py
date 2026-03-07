54dbb00850df632a46a64bbb7db306e621cc4650
Jesse Wattenbarger <jwatt@broken.watch>
2026-03-07 17:28:48 -0500

    Add docstrings to test modules
    - Add docstrings to test modules
    - Make linting make rule compatible with emacs' compilation mode
    - Remove temporarily disabling lint warnings on missing docstrings

--- tests/test_yooxnas.py
+++ tests/test_yooxnas.py
@@ -1,3 +1,4 @@
+"""Tests for yooxnas that fully compile real roms."""
 import logging
 from pathlib import Path
 
@@ -15,7 +16,9 @@ def test_out():
     path.mkdir(exist_ok=True)
     return path
 
+
 def load_and_parse_file(input_filename, output_filename):
+    """Load and parse a tal file."""
     file_path = Path(input_filename)
     out_file_path = Path(output_filename)
     with open(file_path) as asmfile:
@@ -36,26 +39,42 @@ def load_and_parse_file(input_filename, output_filename):
         else:
             pytest.fail("Lexer failed. Parsing skipped.")
 
+
 def test_simple(test_out):
+    """Test a simple rom."""
     load_and_parse_file('examples/simple.tal', test_out / 'simple.rom')
 
+
 def test_hello(test_out):
+    """Test hello world rom."""
     load_and_parse_file('examples/hello.tal', test_out / 'hello.rom')
 
+
 def test_new_hello(test_out):
+    """Test a version of hellow world with newer tal."""
     load_and_parse_file('examples/new_hello.tal', test_out / 'new_hello.rom')
 
+
 def test_fizzbuzz(test_out):
+    """Test fizzbuzz rom."""
     load_and_parse_file('examples/fizzbuzz.tal', test_out / 'fizzbuzz.rom')
 
+
 def test_new_fizzbuzz(test_out):
+    """Test newer version of fizzbuzz."""
     load_and_parse_file('examples/new_fizzbuzz.tal', test_out / 'new_fizzbuzz.rom')
 
+
 def test_includes(test_out):
+    """Test using includes."""
     load_and_parse_file('examples/includes.tal', test_out / 'includes.rom')
 
+
 def test_math32(test_out):
+    """Test building math32.tal."""
     load_and_parse_file('examples/math32.tal', test_out / 'math32.rom')
 
+
 def test_mandelbrot(test_out):
+    """Test building mandelbrot.tal."""
     load_and_parse_file('examples/mandelbrot.tal', test_out / 'mandelbrot.rom')
